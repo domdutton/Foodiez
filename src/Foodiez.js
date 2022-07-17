233d35154632a5931257a4c4e0bf87b621de9487
DomDutton <Ddd94608@gmail.com>
2022-07-17 17:01:02 +0300

    Added about page

--- src/Foodiez.js
+++ src/Foodiez.js
@@ -2,22 +2,25 @@ import React, { Component } from "react";
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import { HashRouter } from "react-router-dom";
 class Foodiez extends Component {
   render() {
     return (
-      <Container>
-        <Navbar  fixed="top" bg="success" variant="dark">
+      <HashRouter>
         <Container>
-          <Navbar.Brand><strong>Foodiez</strong></Navbar.Brand>
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Recipe Catalogue</Nav.Link>
-              <Nav.Link href="#features">About Us</Nav.Link>
-              <Nav.Link href="#pricing">Contact Us</Nav.Link>
-            </Nav>
-        </Container>
-      </Navbar>
+          <Navbar  fixed="top" bg="success" variant="dark">
+          <Container>
+            <Navbar.Brand><strong>Foodiez</strong></Navbar.Brand>
+              <Nav className="me-auto">
+                <Nav.Link href="#home">Recipe Catalogue</Nav.Link>
+                <Nav.Link href="#features">About Us</Nav.Link>
+                <Nav.Link href="#pricing">Contact Us</Nav.Link>
+              </Nav>
+          </Container>
+        </Navbar>
 
-    </Container>
+      </Container>
+    </HashRouter>
     );
   }
 }
